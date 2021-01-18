bba95f6ecc4b2c19dbf2018e5f0763238b4edc95
Edmilson <edmilsonwp@gmail.com>
2021-01-18 15:53:25 -0300

    search is ok

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+const li = document.querySelectorAll('li')
+const input = document.querySelector('#input')
+
+
+const planFilter = e => {
+	const strings = e.target.value.split(' ')
+	
+	//strings.every()
+
+
+	li.forEach(li => {
+		if (li.children[0].children[3].innerText.toLowerCase().includes(e.target.value.toLowerCase())) li.style.display = 'grid'
+		else li.style.display = 'none'
+	})
+}
+
+input.addEventListener('input', planFilter)
+
 /*
 const li = document.querySelectorAll('li .description')
 
 if (li[0].innerText !== '') li[0].parentElement.style.display = 'none'
+
+
+
+
+
+const li = document.querySelectorAll('li')
+li.forEach( li =>{
+    if (!li.children[0].children[3].innerText.includes('Amil')) li.style.display = 'none'
+})
 */
