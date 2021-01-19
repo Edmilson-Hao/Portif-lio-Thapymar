9b47ab993d8d14077776f6eaf1c29faa4326faa7
edmilson <edmilsonwp>
2021-01-19 13:35:34 -0300

    almost finished

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-const li = document.querySelectorAll('li')
-const input = document.querySelector('#input')
+	const li = document.querySelectorAll('li')
+	const input = document.querySelector('#input')
 
 
-const planFilter = e => {
-	const strings = e.target.value.split(' ')
-	
-	//strings.every()
+	const planFilter = e => {
+		//const strings = e.target.value.split(' ')
+		const searchString = e.target.value.toLowerCase()
+				
+		li.forEach(li => {
+			let spanDescription = li.children[0].children[3].innerText.toLowerCase()
+
+			if (spanDescription.includes(searchString)) li.style.display = 'grid'
+			else li.style.display = 'none'
+		})
+	}
+
+	input.addEventListener('input', planFilter)
+
+	/*
+	const li = document.querySelectorAll('li .description')
+
+	if (li[0].innerText !== '') li[0].parentElement.style.display = 'none'
 
 
-	li.forEach(li => {
-		if (li.children[0].children[3].innerText.toLowerCase().includes(e.target.value.toLowerCase())) li.style.display = 'grid'
-		else li.style.display = 'none'
+
+
+
+	const li = document.querySelectorAll('li')
+	li.forEach( li =>{
+		if (!li.children[0].children[3].innerText.includes('Amil')) li.style.display = 'none'
 	})
-}
+	*/
 
-input.addEventListener('input', planFilter)
-
-/*
-const li = document.querySelectorAll('li .description')
-
-if (li[0].innerText !== '') li[0].parentElement.style.display = 'none'
-
-
-
-
-
-const li = document.querySelectorAll('li')
-li.forEach( li =>{
-    if (!li.children[0].children[3].innerText.includes('Amil')) li.style.display = 'none'
-})
-*/
+	//Styles
+	document.querySelector("body").style.backgroundColor = '#d31313'
