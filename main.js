f25cf47121c04688020df0a8a78f9782513721a9
edmilson <edmilsonwp>
2021-01-19 13:45:27 -0300

    almost finished

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 	const li = document.querySelectorAll('li')
 	const input = document.querySelector('#input')
 
+	const clearDisplay = () => {
+		li.forEach( li => {
+			li.style.display = 'none'
+		})
+	}
 
 	const planFilter = e => {
 		//const strings = e.target.value.split(' ')
 		const searchString = e.target.value.toLowerCase()
-				
+		
 		li.forEach(li => {
 			let spanDescription = li.children[0].children[3].innerText.toLowerCase()
 
 			if (spanDescription.includes(searchString)) li.style.display = 'grid'
 			else li.style.display = 'none'
 		})
+
+		if (searchString === '') clearDisplay()
+
 	}
 
 	input.addEventListener('input', planFilter)
 
+	clearDisplay()
 	/*
 	const li = document.querySelectorAll('li .description')
 
